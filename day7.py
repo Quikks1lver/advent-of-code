6aaa74eb4a1d7885abe5acd15743af6d2dc91435
Adam Fernandes <fernadam8@gmail.com>
2020-12-07 10:21:31 -0500

    day 7 pt 2 solved

--- day7.py
+++ day7.py
@@ -4,9 +4,9 @@ from Helpers.FileHelper import readFile
 from typing import List, Dict
 FILEPATH: str = "Input/day7.txt"
 
-def createBagMap(s: str) -> Dict[str, List[str]]:
+def createBagMap(s: str, keepNumbering: bool) -> Dict[str, List[str]]:
    """
-   Creates a bag map from the given string
+   Creates a bag map from the given string, choose to keep or remove numbering
    """
    s = s[0:-2].strip()
    containIndex: int = s.index("contain")
@@ -14,23 +14,29 @@ def createBagMap(s: str) -> Dict[str, List[str]]:
    key: str = s[0:containIndex].strip()[0:-1]
    value: List[str] = s[containIndex+len("contain"):].split(", ")
 
-   # Chomps off number at beginning of string
+   # Chomps off number at beginning of string, if user wants
    for i in range(len(value)):
       tempStr: str = value[i]
       for char in range(len(tempStr)):
-         if tempStr[char] == ' ' or tempStr[char].isdigit():
-            continue
+         if not keepNumbering:
+            if tempStr[char] == ' ' or tempStr[char].isdigit():
+               continue
+            else:
+               break
          else:
-            break
+            if tempStr[char] == ' ':
+               continue
+            else:
+               break
       value[i] = tempStr[char:] if tempStr[-1] != "s" else tempStr[char:-1]
 
    output: Dict[str, List[str]] = dict()
    output[key] = value
    return output
 
-def recursiveGoldCounter(key, bagMap) -> int:
+def recursiveGoldCounter(key: str, bagMap: Dict[str, List[str]]) -> int:
    """
-   Recursively sees if the key (a bag) has any gold inside it
+   Recursively sees if the key (a bag) has any gold inside it. Part 1
    """
    flag: int = 0
 
@@ -47,6 +53,19 @@ def recursiveGoldCounter(key, bagMap) -> int:
 
    return 1 if flag >= 1 else 0
 
+def countNumBagsWithin(key: str, bagMap: Dict[str, List[str]]) -> int:
+   """
+   Counts number of bags within the key (a bag). Part 2
+   """
+   count: int = 0
+
+   for val in bagMap[key]:
+      if val != "no other bag":
+         numSubBag: int = int(val[0])
+         count += numSubBag + (numSubBag * countNumBagsWithin(val[2:], bagMap))
+   
+   return count
+
 def main():
    # Capture input
    input: List[str] = readFile(FILEPATH)
@@ -54,13 +73,48 @@ def main():
 
    # Populate hash map of bags
    for s in input:
-      bagMap.update(createBagMap(s))
+      bagMap.update(createBagMap(s, False))
 
    # Part 1
    count = 0
    for key in bagMap.keys():
       count += recursiveGoldCounter(key, bagMap)
-   print(f"Part 1 -- Shiny Gold Bag Count: {count - 1}")
+   print(f"Part 1 -- Shiny Gold Bag count: {count - 1}")
+
+   # Part 2
+   bagMap = bagMap.clear()
+   bagMap = {}
+   for s in input:
+      bagMap.update(createBagMap(s, True))
+   numBagsWithin: int = countNumBagsWithin("shiny gold bag", bagMap)
+   print(f"Part 2 -- number of bags within Shiny Gold Bag: {numBagsWithin}")
 
 if __name__ == "__main__":
    main()
+
+"""
+--- Day 7: Handy Haversacks ---
+--- Part One ---
+In the above rules, the following options would be available to you:
+
+    A bright white bag, which can hold your shiny gold bag directly.
+    A muted yellow bag, which can hold your shiny gold bag directly, plus some other bags.
+    A dark orange bag, which can hold bright white and muted yellow bags, either of which could then hold your shiny gold bag.
+    A light red bag, which can hold bright white and muted yellow bags, either of which could then hold your shiny gold bag.
+So, in this example, the number of bag colors that can eventually contain at least one shiny gold bag is 4.
+How many bag colors can eventually contain at least one shiny gold bag? (The list of rules is quite long; make sure you get all of it.)
+--- Part Two ---
+Consider again your shiny gold bag and the rules from the above example:
+
+    faded blue bags contain 0 other bags.
+    dotted black bags contain 0 other bags.
+    vibrant plum bags contain 11 other bags: 5 faded blue bags and 6 dotted black bags.
+    dark olive bags contain 7 other bags: 3 faded blue bags and 4 dotted black bags.
+
+So, a single shiny gold bag must contain 1 dark olive bag (and the 7 bags within it) plus 2 vibrant
+plum bags (and the 11 bags within each of those): 1 + 1*7 + 2 + 2*11 = 32 bags!
+
+In this example, a single shiny gold bag must contain 126 other bags.
+
+How many individual bags are required inside your single shiny gold bag?
+"""
