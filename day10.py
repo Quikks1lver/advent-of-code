a2aa94f0a60b1077c3dc1cd862bc14c6e947bcc9
Adam Fernandes <fernadam8@gmail.com>
2020-12-10 09:56:41 -0500

    day 10 pt solved

--- day10.py
+++ day10.py
@@ -25,46 +25,48 @@ def calculateVoltageDifference(adapters: List[int]) -> int:
    product: int = oneDiff * (threeDiff + 1) # don't forget your own adapter
    return product
 
-def countValidArrangements(adapters: List[int], prev: int) -> int:
+def countValidArrangements(adapters: List[int]) -> int:
    """
-   Using backtracking, counts valid arrangements of the adapters
+   Counts valid arrangements of the adapters (sorted list)
    """
-   # Base cases
-   print(prev)
-   if len(adapters) <= 0:
-      return 1
-   if abs(adapters[0] - prev) > 3:
-      return 0
+   paths: List[int] = [0] * (adapters[-1] + 1)
+   paths[0] = 1
+
+   for index in range(1, adapters[-1] + 1):
+      for i in range(1, 4):
+         if index - i in adapters:
+            paths[index] += paths[index - i]
    
-   valid: int = 1
-   count: int = 0
-   for i in range(len(adapters)):
-      tempCount: int = countValidArrangements(adapters[i + 1:], adapters[i])
-      if valid and tempCount:
-         count += tempCount
-      else:
-         break
-   
-   return count
+   return paths[-1]
 
 def main():
    adapters: List[int] = [int(s.strip()) for s in readFile(FILEPATH)]
+   adapters.append(0) # outlet
    adapters.sort()
 
    # Part 1
    diff: int = calculateVoltageDifference(adapters)
    print(f"Part 1 -- Diff: {diff}")
    
-   # (my attempt at) Part 2
-   # numWays: int = countValidArrangements(adapters, 0)
-   print(f"Part 2 -- Numways: to be continued . . . xD")
+   # Part 2
+   # I was stumped on this problem, so I looked up a video to help me understand dynamic programming,
+   # something I have never learned or tackled before. This video is what helped me solve the problem:
+   # https://www.youtube.com/watch?v=eeYanhLamjg.
+   numWays: int = countValidArrangements(adapters)
+   print(f"Part 2 -- Numways: {numWays}")
 
 if __name__ == "__main__":
    main()
 
 """
-
+--- Day 10: Adapter Array ---
 --- Part One ---
-
+If you use every adapter in your bag at once, what is the distribution of joltage differences between
+the charging outlet, the adapters, and your device?
+Find a chain that uses all of your adapters to connect the charging outlet to your device's built-in
+adapter and count the joltage differences between the charging outlet, the adapters, and your device.
+What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?
 --- Part Two ---
+What is the total number of distinct ways you can arrange the adapters to connect the charging outlet
+to your device?
 """
