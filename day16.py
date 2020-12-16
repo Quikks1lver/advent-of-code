9cd0e4ddd0b819c3006167b4884b4ddc53f473e0
Adam Fernandes <fernadam8@gmail.com>
2020-12-16 10:00:19 -0500

    day 16 directions

--- day16.py
+++ day16.py
@@ -92,8 +92,26 @@ if __name__ == "__main__":
    main()
 
 """
-
+--- Day 16: Ticket Translation ---
 --- Part One ---
-
+You collect the rules for ticket fields, the numbers on your ticket, and the numbers on other nearby
+tickets for the same train service (via the airport security cameras) together into a single document
+you can reference (your puzzle input).
+The rules for ticket fields specify a list of fields that exist somewhere on the ticket and the valid
+ranges of values for each field. For example, a rule like class: 1-3 or 5-7 means that one of the fields
+in every ticket is named class and can be any value in the ranges 1-3 or 5-7 (inclusive, such that 3
+and 5 are both valid in this field, but 4 is not).
+Start by determining which tickets are completely invalid; these are tickets that contain values which
+aren't valid for any field. Ignore your ticket for now.
+It doesn't matter which position corresponds to which field; you can identify invalid nearby tickets
+by considering only whether tickets contain values that are not valid for any field.
+Consider the validity of the nearby tickets you scanned. What is your ticket scanning error rate?
 --- Part Two ---
+Now that you've identified which tickets contain invalid values, discard those tickets entirely. Use
+the remaining valid tickets to determine which field is which.
+Using the valid ranges for each field, determine what order the fields appear on the tickets. The order
+is consistent between all tickets: if seat is the third field, it is the third field on every ticket,
+including your ticket.
+Once you work out which field is which, look for the six fields on your ticket that start with the word
+departure. What do you get if you multiply those six values together?
 """
