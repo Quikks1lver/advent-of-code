b5f723296b9fd8d3ad0c96d0cc1a0012c355e7ec
Adam Fernandes <fernadam8@gmail.com>
2020-12-23 11:00:54 -0500

    more cleanup

--- day23.py
+++ day23.py
@@ -34,6 +34,7 @@ class Cups:
       Simulates moves for x rounds. Modifies game's numbers list in place
       """
       if roundNum <= 0:
+         print(f"Moving {roundNum} round(s) -> invalid")
          return
       
       currentIndex, currentNum = 0, 0
@@ -72,7 +73,7 @@ class Cups:
    def stringify(self, x: int) -> str:
       """
       Create a string from the specified number in the list to the end.
-      If the number is not there, simply stringifies what is currently in the list.
+      If the number is not there, simply stringifies what is currently in the list
       """
       s: str = ""
       index: int = 0
@@ -99,7 +100,7 @@ def main():
    
    # Part 1
    game.move(100)
-   print(game.stringify(1)[1:])
+   print(f"Part 1 -- Cups order after cup #1: {game.stringify(1)[1:]}")
 
    # Part 2
 
@@ -127,4 +128,18 @@ Each move, the crab does the following actions:
     - The crab selects a new current cup: the cup which is immediately clockwise of the current cup.
 Using your labeling, simulate 100 moves. What are the labels on the cups after cup 1?
 --- Part Two ---
+Due to what you can only assume is a mistranslation (you're not exactly fluent in Crab), you are quite
+surprised when the crab starts arranging many cups in a circle on your raft - one million (1000000) in total.
+Your labeling is still correct for the first few cups; after that, the remaining cups are just numbered
+in an increasing fashion starting from the number after the highest number in your list and proceeding
+one by one until one million is reached. (For example, if your labeling were 54321, the cups would be
+numbered 5, 4, 3, 2, 1, and then start counting up from 6 until one million is reached.) In this way,
+every number from one through one million is used exactly once.
+After discovering where you made the mistake in translating Crab Numbers, you realize the small crab
+isn't going to do merely 100 moves; the crab is going to do ten million (10000000) moves!
+The crab is going to hide your stars - one each - under the two cups that will end up immediately
+clockwise of cup 1. You can have them if you predict what the labels on those cups will be when the crab
+is finished.
+In the above example (389125467), this would be 934001 and then 159792; multiplying these together produces 149245887792.
+Determine which two cups will end up immediately clockwise of cup 1. What do you get if you multiply their labels together?
 """
