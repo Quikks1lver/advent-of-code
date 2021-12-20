f99c51da0b9f038820bb84f68abec993c4065010
Adam Fernandes <fernadam8@gmail.com>
2021-12-19 22:13:34 -0500

    day 8 pt1

--- 2021/day08.py
+++ 2021/day08.py
@@ -1,12 +1,34 @@
+from functools import reduce
 from Helpers.FileHelpers import read_lines
-from typing import List
+from typing import List, Tuple
 FILEPATH = "2021/Input/day08.txt"
 
-def main():
-   input_lines = [line for line in read_lines(FILEPATH)]
+SEV_SEG_EZ_NUM_LENGTHS = {2, 3, 4, 7}
 
-   print(f"Part 1 -- {}")
-   print(f"Part 2 -- {}")
+def parse_input(input_lines: List[str]) -> Tuple[List[List[str]], List[List[str]]]:
+   signal_patterns: List[List[str]] = []
+   output_values: List[List[str]] = []
+
+   for _ in input_lines:
+      s = _.split("|")
+      signal_patterns.append(s[0].split())
+      output_values.append(s[1].split())
+
+   return signal_patterns, output_values
+
+def count_easy_digits(output_values: List[List[str]]) -> int:
+   count = 0
+   for row in output_values:
+       for string in row:
+          if len(string) in SEV_SEG_EZ_NUM_LENGTHS:
+             count += 1
+   return count
+
+def main():
+   signal_patterns, output_values = parse_input([line.strip() for line in read_lines(FILEPATH)])
+
+   print(f"Part 1 -- {count_easy_digits(output_values)}")
+   # print(f"Part 2 -- {}")
 
 if __name__ == "__main__":
    main()
